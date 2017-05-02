a5aa9677109bf0374838c713371952c6439e1138
biros95 <biros1995@gmail.com>
2017-05-02 19:25:06 +0200

    Texture lo recuperamos desde asset manager
    Inicio de creacion de jugadores en la PlayScreen.

--- core/src/com/mygdx/game/screens/PlayScreen.java
+++ core/src/com/mygdx/game/screens/PlayScreen.java
@@ -1,6 +1,8 @@
 package com.mygdx.game.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
@@ -10,21 +12,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.game.AirHockey;
 import com.mygdx.game.helpers.MyAssetManager;
+
+import sun.rmi.runtime.Log;
 
 /**
  * Created by MarcosPortatil on 18/04/2017.
  */
 
-public class PlayScreen extends BaseScreen {
+public class PlayScreen extends BaseScreen implements InputProcessor {
     //Creamos el campo y los dos jugadores.
     Sprite pista, player, player2;
 
     SpriteBatch batch;
     private int height;
     private int width;
+    private float posX;
+    private float posY;
     MyAssetManager myAssetManager;
 
     OrthographicCamera camera;
@@ -33,6 +40,7 @@ public class PlayScreen extends BaseScreen {
     public PlayScreen(AirHockey game) {
         super(game);
         myAssetManager = new MyAssetManager();
+        Gdx.input.setInputProcessor(this);
     }
 
 
@@ -48,7 +56,9 @@ public class PlayScreen extends BaseScreen {
         player2 = myAssetManager.cargarTextura("player");
 
         //Posiciones de los jugadores.
-        player.setPosition(width / 2, height / 4);
+        posX = height / 4;
+        posY = width / 2;
+        player.setPosition(posY, posX);
         player2.setPosition(width/2, (height/4)*3);
         pista.setSize(width, height);
 
@@ -57,9 +67,62 @@ public class PlayScreen extends BaseScreen {
     }
 
     @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        Gdx.app.log("MyTag", "my informative message");
+        if(button == Buttons.LEFT){
+            posX = screenX - player.getWidth()/2;
+            posY = Gdx.graphics.getHeight() - screenY - player.getHeight()/2;
+        }
+        if(button == Buttons.RIGHT){
+            posX = Gdx.graphics.getWidth()/2 - player.getWidth()/2;
+            posY = Gdx.graphics.getHeight()/2 - player.getHeight()/2;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        Gdx.app.log("HOLA", "HOLA");
+
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+
+    @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        player.setPosition(posX,posY);
         batch.begin();
         pista.draw(batch);
         player.draw(batch);
